f1f479524aeeec108a9385c56ea4389c8dee90cf
yahelroro0196 <63183261+yahelroro0196@users.noreply.github.com>
2020-04-06 21:44:57 +0300

    implemented: 1. clearer variable types in methods.

--- prerace_setup.py
+++ prerace_setup.py
@@ -12,7 +12,7 @@ RACE_SETTINGS = 'race_settings'
 ROUNDS = 'rounds'
 
 
-def initialize_rounds(race_settings) -> dict:
+def initialize_rounds(race_settings: dict) -> dict:
     rounds = {}
     for i, curr_round in enumerate(race_settings):
         racers = _build_round_racers(curr_round, race_settings)
@@ -25,12 +25,12 @@ def load_race_settings() -> dict:
         return json.load(cfg_file)[RACE_SETTINGS]
 
 
-def unpack_round_settings(curr_round, rounds):
+def unpack_round_settings(curr_round: int, rounds: dict):
     round_settings = rounds[curr_round]
     return round_settings[RACERS], round_settings[TRACK_LENGTH]
 
 
-def _build_round(curr_round, i, race_settings, racers, rounds):
+def _build_round(curr_round, i, race_settings: dict, racers, rounds):
     rounds[str(i + ORIGIN0_OFFSET)] = {
         TRACK_LENGTH: race_settings[curr_round][TRACK_LENGTH],
         RACERS: racers
@@ -38,13 +38,13 @@ def _build_round(curr_round, i, race_settings, racers, rounds):
     return rounds
 
 
-def _build_round_racers(curr_round, race_settings):
+def _build_round_racers(curr_round: int, race_settings: dict):
     racers_settings = race_settings[curr_round][RACERS]
     racers = [_build_racer(racer_name, racers_settings) for racer_name in racers_settings]
     return racers
 
 
-def _build_racer(racer_name, racers_settings):
+def _build_racer(racer_name: str, racers_settings: dict):
     racer = racers_settings[racer_name]
     racer_type = racer[ANIMAL_TYPE]
     del racer[ANIMAL_TYPE]
